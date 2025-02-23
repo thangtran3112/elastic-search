8ce87eb2694bb60a802c98abeb243ff227c9923f
Toby Tran <ttran@stackline.com>
2025-02-23 12:13:34 -0800

    add query documents by year

--- final_project/final_code/backend/main.py
+++ final_project/final_code/backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 
 from config import INDEX_NAME_DEFAULT
 from utils import get_es_client
@@ -17,33 +18,97 @@ app.add_middleware(
 
 
 @app.get("/api/v1/regular_search/")
-async def search(search_query: str, skip: int = 0, limit: int = 10) -> dict:
-    es = get_es_client(max_retries=1, sleep_time=0)
-    query = {
-        "bool": {
-            "must": [
-                {
-                    "multi_match": {
-                        "query": search_query,
-                        "fields": ["title", "explanation"],
+async def search(
+    search_query: str,
+    skip: int = 0,
+    limit: int = 10,
+    year: str | None = None,
+) -> dict:
+    try:
+        es = get_es_client(max_retries=1, sleep_time=0)
+        query = {
+            "bool": {
+                "must": [
+                    {
+                        "multi_match": {
+                            "query": search_query,
+                            "fields": [
+                                "title",
+                                "explanation",
+                            ],  # search_query text within title and explanation fields
+                        }
                     }
-                }
-            ]
+                ]
+            }
         }
-    }
-    response = es.search(
-        index=INDEX_NAME_DEFAULT,
-        body={
-            "query": query,
-            "from": skip,
-            "size": limit,
-        },
-        filter_path=[
-            "hits.hits._source",
-            "hits.hits._score",
-            "hits.total",
-        ],
-    )
-    return {
-        "hits": response["hits"]["hits"],
-    }
+        response = es.search(
+            index=INDEX_NAME_DEFAULT,
+            body={
+                "query": query,
+                "from": skip,
+                "size": limit,
+            },
+            filter_path=[
+                "hits.hits._source",
+                "hits.hits._score",
+                "hits.total",
+            ],
+        )
+        return {
+            "hits": response["hits"]["hits"],
+        }
+    except Exception as e:
+        return handle_error(e)
+
+
+@app.get("/api/v1/get_docs_per_year_count/")
+async def get_docs_per_year_count(search_query: str) -> dict:
+    try:
+        es = get_es_client(max_retries=1, sleep_time=0)
+        query = {
+            "bool": {
+                "must": [
+                    {
+                        "multi_match": {
+                            "query": search_query,
+                            "fields": [
+                                "title",
+                                "explanation",
+                            ],  # search_query text within title and explanation fields
+                        }
+                    }
+                ]
+            }
+        }
+        index_name = INDEX_NAME_DEFAULT
+        response = es.search(
+            index=index_name,
+            body={
+                "query": query,
+                "aggs": {
+                    "docs_per_year": {
+                        "date_histogram": {  # https://www.elastic.co/guide/en/elasticsearch/reference/current/search-aggregations-bucket-datehistogram-aggregation.html
+                            "field": "date",
+                            "calendar_interval": "year",  # Group by year
+                            "format": "yyyy",  # Format the year in the response
+                        }
+                    }
+                },
+            },
+            filter_path=["aggregations.docs_per_year"],
+        )
+        return {"docs_per_year": extract_docs_per_year(response)}
+    except Exception as e:
+        return handle_error(e)
+
+
+def extract_docs_per_year(response: dict) -> dict:
+    aggregations = response.get("aggregations", {})
+    docs_per_year = aggregations.get("docs_per_year", {})
+    buckets = docs_per_year.get("buckets", [])
+    return {bucket["key_as_string"]: bucket["doc_count"] for bucket in buckets}
+
+
+def handle_error(e: Exception) -> HTMLResponse:
+    error_message = f"An error occurred: {str(e)}"
+    return HTMLResponse(content=error_message, status_code=500)
